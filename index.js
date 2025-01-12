dc21299998c718df303a4138c2b142a209c9c4b2
banda59 <proud@outlook.kr>
2025-01-13 02:12:43 +0900

    commit

--- index.js
+++ index.js
@@ -18,17 +18,29 @@ const parser = new Parser({
   const feed = await parser.parseURL("https://spacefriend.tistory.com/rss");
 
   // 인사말과 GIF 추가
-  const headerSection = `# Hi, I'm banda 👋\n\n![chipi](https://github.com/banda59/README/blob/main/gif/chipi-chipi-chapa-chapa.gif)\n\n`;
+  const headerSection = `# Hi, I'm banda 👋\n\n![chipi](https://github.com/banda59/README/blob/main/gif/chipi-chipi-chapa-chapa.gif)\n`;
 
   // 최신 5개의 글의 제목과 링크를 추가할 텍스트 생성
-  let latestPosts = "## Latest Blog Posts 😽🔐\n\n";
+  let latestPosts = "### Latest Blog Posts\n\n";
   for (let i = 0; i < 5 && i < feed.items.length; i++) {
     const { title, link } = feed.items[i];
     latestPosts += `- [${title}](${link})\n`;
   }
 
-  // 전체 내용 생성
-  const fullContent = `${headerSection}${latestPosts}`;
+  // 기존 README.md에 최신 블로그 포스트 추가
+  if (readmeContent.includes("### Latest Blog Posts")) {
+    // 기존 섹션을 새 내용으로 대체
+    readmeContent = readmeContent.replace(
+      /### Latest Blog Posts[\s\S]*?(?=\n\n## |\n$)/,
+      latestPosts
+    );
+  } else {
+    // 새 섹션 추가
+    readmeContent += `\n${latestPosts}`;
+  }
+
+  // 전체 내용 작성
+  const fullContent = `${headerSection}\n${readmeContent}`;
 
   // 기존 README를 완전히 대체
   writeFileSync(readmePath, fullContent, "utf8");
