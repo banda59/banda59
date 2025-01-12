9e740213725392494cdc7f61455332be1d9fb859
banda59 <proud@outlook.kr>
2025-01-13 02:06:31 +0900

    commit

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ const parser = new Parser({
   },
 });
 
-// 최신 블로그 포스트와 GIF 추가하는 함수
+// 최신 블로그 포스트와 섹션 추가하는 함수
 (async () => {
   // RSS 피드 가져오기
   const feed = await parser.parseURL("https://spacefriend.tistory.com/rss");
 
   // 인사말과 GIF 추가
-  const greeting = `# Hi, I'm banda 👋\n---\n\n![chipi](https://github.com/banda59/README/blob/main/gif/chipi-chipi-chapa-chapa.gif)\n\n`;
+  const headerSection = `# Hi, I'm banda 👋\n---\n\n![chipi](https://github.com/banda59/README/blob/main/gif/chipi-chipi-chapa-chapa.gif)\n\n`;
 
   // 최신 5개의 글의 제목과 링크를 추가할 텍스트 생성
   let latestPosts = "### Latest Blog Posts\n---\n\n";
@@ -27,18 +27,11 @@ const parser = new Parser({
     latestPosts += `- [${title}](${link})\n`;
   }
 
-  // 인사말과 블로그 포스트 섹션을 모두 업데이트
-  if (!readmeContent.startsWith(greeting)) {
-    readmeContent = greeting + readmeContent; // 인사말과 GIF 추가
-  }
+  // 전체 내용 생성
+  const fullContent = `${headerSection}${latestPosts}`;
 
-  // 기존 "Latest Blog Posts" 섹션 대체
-  const regex = /### Latest Blog Posts[\s\S]*?(?=\n\n## |\n$|$)/;
-  const newReadmeContent = regex.test(readmeContent)
-    ? readmeContent.replace(regex, latestPosts)
-    : readmeContent + "\n\n" + latestPosts;
+  // 기존 README를 완전히 대체
+  writeFileSync(readmePath, fullContent, "utf8");
 
-  // 변경된 내용 저장
-  writeFileSync(readmePath, newReadmeContent, "utf8");
   console.log("README.md 업데이트 완료");
 })();
